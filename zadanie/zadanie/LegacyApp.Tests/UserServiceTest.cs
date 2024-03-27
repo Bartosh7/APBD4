c6a612ef82b249fdfc66c171a544b57342c2cf55
s27471 <s27471@pjwstk.edu.pl>
2024-03-27 16:48:59 +0100

    commit

---
+++ zadanie/zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using JetBrains.Annotations;
+using LegacyApp;
+using Xunit;
+
+namespace LegacyApp.Tests;
+
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+    [Fact]
+    public void AddUser_Should_Return_True_When_AllData_Is_Correct()
+    {
+        //Arrange
+        var userService = new UserService();
+        //Act
+        var addResult = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert
+        Assert.True(addResult);
+    }
+    [Fact]
+    public void AddUser_Should_Return_False_When_Client_Is_Under21()
+    {
+        //Arrange
+        var userService = new UserService();
+        //Act
+        var addResult = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("2020-04-21"), 1);
+        //Assert
+        Assert.False(addResult);
+    }
+    
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Is_Missing()
+    {
+        //Arrange
+        var userService = new UserService();
+        //Act
+        var addResult = userService.AddUser("", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert
+        Assert.False(addResult);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_Surname_Is_Missing()
+    {
+        //Arrange
+        var userService = new UserService();
+        //Act
+        var addResult = userService.AddUser("John", "", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert
+        Assert.False(addResult);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_Email_Is_Missing_AtSign_Or_Dot()
+    {
+        //Arrange
+        var userService = new UserService();
+        //Act
+        var addResult = userService.AddUser("John", "Doe", "johndoegmailcom", DateTime.Parse("1982-03-21"), 1);
+        //Assert
+        Assert.False(addResult);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_Client_Is_Not_VeryImportant()
+    {
+        // Arrange
+        var client = new Client { Type = "ImportantClient" };
+
+        // Act
+        bool result = UserService.IsClientVeryImportant(client);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_Client_Is_Not_Important()
+    {
+        // Arrange
+        var client = new Client { Type = "VeryImportantClient" };
+
+        // Act
+        bool result = UserService.IsClientImportant(client);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void SettingCreditLimit_WhenClientIsVeryImportant_ShouldUserCreditLimitBeFalse()
+    {
+        // Arrange
+        var client = new Client { Type = "VeryImportantClient" };
+        var user = UserService.UserCreator("John", "Doe", "john.doe@gmail.com", DateTime.Parse("1982-03-21"), client);
+
+        // Act
+        UserService.SettingCreditLimit(client, user);
+
+        // Assert
+        Assert.False(user.HasCreditLimit);
+    }
+
+    [Fact]
+    public void SettingCreditLimit_WhenClientIsImportant_ShouldSetUserCreditLimitTwoTimeHigherThanNormalClient()
+    {
+        // Arrange
+        var client = new Client { Type = "ImportantClient" };
+        var user = UserService.UserCreator("John", "Doe", "john.doe@gmail.com", DateTime.Parse("1982-03-21"), client);
+        UserService.ChangeCreditLimitToNormalClient(user);
+        var limitBefore = user.CreditLimit;
+
+
+        // Act
+        UserService.SettingCreditLimit(client, user);
+
+        // Assert
+        Assert.Equal(limitBefore * 2, user.CreditLimit);
+    }
+
+    [Fact]
+    public void SettingCreditLimit_WhenClientIsNoramal_ShouldUserCreditLimitBeTrue()
+    {
+        // Arrange
+        var client = new Client { Type = "NormalClient" };
+        var user = UserService.UserCreator("John", "Doe", "john.doe@gmail.com", DateTime.Parse("1982-03-21"), client);
+        
+        // Act
+        UserService.SettingCreditLimit(client, user);
+
+        // Assert
+        Assert.True(user.HasCreditLimit);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_User_Limit_Is_Lower_Than_500()
+    {
+        //Arrange
+        var userService = new UserService();
+        //Act - Kowalski ma limit 200
+        var addResult = userService.AddUser("John", "Kowalski", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert 
+        Assert.False(addResult);
+    }
+    
+    
+}
---
+++ zadanie/zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (IsNameCorrect(firstName) || IsSurnameCorrect(lastName))
+            {
+                return false;
+            }
+
+            if (IsEmailInCorrect(email))
+            {
+                return false;
+            }
+
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = UserCreator(firstName, lastName, email, dateOfBirth, client);
+
+            SettingCreditLimit(client, user);
+
+            if (!IsCreditLimitHighEnough(user)) return false;
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        public static void SettingCreditLimit(Client client, User user)
+        {
+            if (IsClientVeryImportant(client))
+            {
+                ChangeCreditLimitToVeryImportantClient(user);
+            }
+            else if (IsClientImportant(client))
+            {
+                    ChangeCreditLimitToImportantClient(user);
+            }
+            else
+            {
+                ChangeCreditLimitToNormalClient(user);
+            }
+        }
+
+
+        public static User UserCreator(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+            return user;
+        }
+
+        public static void ChangeCreditLimitToNormalClient(User user)
+        {
+            user.HasCreditLimit = true;
+            using (var userCreditService = new UserCreditService())
+            {
+                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                user.CreditLimit = creditLimit;
+            }
+        }
+
+        public static void ChangeCreditLimitToVeryImportantClient(User user)
+        {
+            user.HasCreditLimit = false;
+        }
+
+        public static void ChangeCreditLimitToImportantClient(User user)
+        {
+            using (var userCreditService = new UserCreditService())
+            {
+                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                creditLimit = creditLimit * 2;
+                user.CreditLimit = creditLimit;
+            }
+        }
+
+        public static bool IsCreditLimitHighEnough(User user)
+        {
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        
+        public static bool IsClientImportant(Client client)
+        {
+            return client.Type == "ImportantClient";
+        }
+
+        public static bool IsClientVeryImportant(Client client)
+        {
+            return client.Type == "VeryImportantClient";
+        }
+
+        private static bool IsEmailInCorrect(string email)
+        {
+            return !email.Contains("@") && !email.Contains(".");
+        }
+
+        private static bool IsSurnameCorrect(string lastName)
+        {
+            return string.IsNullOrEmpty(lastName);
+        }
+
+        private static bool IsNameCorrect(string firstName)
+        {
+            return string.IsNullOrEmpty(firstName);
+        }
+    }
+}
